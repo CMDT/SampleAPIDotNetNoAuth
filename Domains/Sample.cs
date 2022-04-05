670b3c5fe041087a12010f0ee933ad320eda1d56
Josh Peake <joshpeake@live.com>
2022-04-05 11:35:14 +0100

    Inital Commit

---
+++ Domains/Sample.cs
@@ -0,0 +1,58 @@
+﻿using api.Domains.Interfaces;
+using api.DTOs;
+using api.Models;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System;
+using Microsoft.Extensions.Configuration;
+
+
+namespace api.Domains
+{
+    public class Sample : ISample // A specialised domain that wraps Auth0 APIs
+    {
+        private readonly string domain;
+
+        private readonly Context context;
+
+        private readonly ILogger logger;
+
+        public Sample(Context _context, ILogger<Sample> _logger)
+        {
+            domain = Environment.GetEnvironmentVariable("AUTH0_DOMAIN");
+
+            context = _context;
+
+            logger = _logger;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void DeleteItem(string id)
+        {
+            throw new NotImplementedException();
+        } 
+
+        public List<SampleDTO> ReadItems()
+        {
+           throw new NotImplementedException();
+        }
+
+        public SampleDTO ReadItem(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public SampleDTO EditItem(SampleDTO item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public SampleDTO AddItem(SampleDTO item)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+
+}
